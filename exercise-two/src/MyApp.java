e17dccf80e5c0ec45151cbc91a2c60bc6c9caf08
Giuliana Fertonani <giulyferto@gmail.com>
2020-07-29 13:04:05 -0700

    Exercise-two

---
+++ exercise-two/src/MyApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Realice un algoritmo para determinar si un número es positivo o negativo.\n" +
+                "Represéntelo en pseudocódigo y diagrama de flujo.\n");
+        //Se definen las variables
+        double number;
+        String result;
+
+        //Se habilita el uso del teclado
+        Scanner keyboard = new Scanner(System.in);
+
+        //Se piden los datos del usuario
+        System.out.print("Ingrese un numero: ");
+        number = keyboard.nextDouble();
+
+        //Se verifica si el numero es positivo o negativo
+        if (number>=0){
+            result = "positivo";
+        } else {
+            result = "negativo";
+        }
+
+        //Se muestra el resultado
+        System.out.println("Su numero es "+result);
+    }
+}
